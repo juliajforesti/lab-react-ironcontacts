f93a0f3dc13371425986f35c64805b3d4d1bce66
Julia Foresti <juliajforesti@gmail.com>
2020-07-23 11:31:41 -0300

    refactored using hooks

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,75 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import contacts from './contacts.json';
-import Contacts from './components/Contacts';
+import React from "react";
+import "./App.css";
+import Contacts from "./components/Contacts";
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      fiveContacts: [...contacts].slice(0, 5),
-      allContacts: [...contacts],
-    }
-
-    this.randomContact = this.randomContact.bind(this);
-    this.sortByName = this.sortByName.bind(this);
-    this.sortByPopularity = this.sortByPopularity.bind(this);
-    this.removeContact = this.removeContact.bind(this);
-
-  }
-
-  randomContact() {
-    const oneContact = this.state.allContacts[Math.floor(Math.random()*this.state.allContacts.length)];
-    const arr = [...this.state.fiveContacts]
-    arr.push(oneContact)
-
-    this.setState({
-        fiveContacts: arr
-    })
-  }
-
-  sortByName() {
-      const arr = [...this.state.fiveContacts];
-      arr.sort((a,b) => a.name.localeCompare(b.name))
-      this.setState({
-        fiveContacts: arr
-    })
-  }
-
-  sortByPopularity() {
-      const arr = [...this.state.fiveContacts]
-      arr.sort((a,b) => b.popularity - a.popularity)
-      this.setState({
-        fiveContacts: arr
-    })
-  }
-
-  removeContact(idx) {
-    const arr = [...this.state.fiveContacts];
-    arr.splice(idx, 1)
-    this.setState({
-      fiveContacts: arr
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-
-        <button onClick={this.randomContact}>Add Random Contact</button>
-        <button onClick={this.sortByName}>Sort by Name</button>
-        <button onClick={this.sortByPopularity}>Sort by Popularity</button>
-
-
-        <Contacts contactList={this.state.fiveContacts} removeContact={this.removeContact}></Contacts>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>IronContacts</h1>
+      <Contacts />
+    </div>
+  );
+};
 
 export default App;
--- starter-code/src/components/Contacts.js
+++ starter-code/src/components/Contacts.js
@@ -1,39 +1,75 @@
-import React, { Component } from 'react';
-import contacts from '../contacts.json';
+import React, { useState } from "react";
+import contacts from "../contacts.json";
 
+const initialState = contacts.slice(0, 5);
 
+const Contacts = () => {
+  const [state, setstate] = useState(initialState);
 
-const Contacts = ({ contactList, removeContact }) => {
-    console.log(contactList)
-    
-    const newList = [...contactList];
-    console.log(newList)
-    return (
-        <div>
-            <h1>IronContacts</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Popularity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {newList.map((elem, idx) => {
-                    return (
-                        <tr key={elem.id}>
-                            <td><img src={elem.pictureUrl} style={{height: '100px'}} alt={elem.name + "'s picture"} /> </td>
-                            <td>{elem.name}</td>
-                            <td>{elem.popularity}</td>
-                            <button onClick={() => removeContact(idx)}>Remove from list</button>
-                        </tr>
-                    )}
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  function handleRandomClick() {
+    const randomNumber = Math.floor(Math.random() * (contacts.length - 5) + 5);
+    if (state.includes(contacts[randomNumber])) return
+    state.push(contacts[randomNumber]);
+
+    return setstate([...state]);
+  }
+
+  function handleSortClick(key) {
+    state.sort((a, b) => {
+      if (key === "name") return a[key].localeCompare(b[key]);
+      return a[key] - b[key];
+    });
+    return setstate([...state])
+  }
+
+  function handleDeleteClick(id) {
+    return setstate([...state.filter((contact) => contact.id !== id)]);
+  }
+
+  return (
+    <div className="container">
+      <div className="btn-container">
+        <button onClick={handleRandomClick}>Add random contact</button>
+        <button onClick={() => handleSortClick("name")}>Sort by name</button>
+        <button onClick={() => handleSortClick("popularity")}>
+          Sort by popularity
+        </button>
+      </div>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.map((contact, idx) => {
+              return (
+                <tr key={contact.id}>
+                  <td>
+                    <img
+                      src={contact.pictureUrl}
+                      style={{ height: "100px" }}
+                      alt=''
+                    />{" "}
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  <td>
+                  <button onClick={() => handleDeleteClick(contact.id)}>
+                    Delete
+                  </button>
+                  </td> 
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Contacts;
